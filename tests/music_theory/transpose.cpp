4e097caafd2481e93336ce545faa5e47a5cc4c15
Robert Patterson <robert@robertgpatterson.com>
2025-03-28 16:04:42 -0500

    Add more robust test of chromatic transposition

--- tests/music_theory/transpose.cpp
+++ tests/music_theory/transpose.cpp
@@ -55,34 +55,89 @@
      EXPECT_EQ(t.displacement(), 1);
  }
  
- TEST(TransposerTest, ChromaticTransposeExamples) {
-     Transposer t(0, 0); // C natural
-     t.chromaticTranspose(2, -1); // Minor third up
-     EXPECT_EQ(t.displacement(), 2);
-     EXPECT_EQ(t.alteration(), -1);
- 
-     t.chromaticTranspose(-2, 1); // Back down a minor third
-     EXPECT_EQ(t.displacement(), 0);
-     EXPECT_EQ(t.alteration(), 0);
- }
- 
- TEST(TransposerTest, Non12EDOEquivalentPitch) {
-     std::vector<int> edo19Major = {0, 3, 6, 8, 11, 14, 17};
-     Transposer t(0, 0, false, 19, edo19Major);
- 
-     // D♭ != C
-     EXPECT_FALSE(t.isEnharmonicEquivalent(1, -2));
-     // but one more flat is equivalent
-     EXPECT_TRUE(t.isEnharmonicEquivalent(1, -3));
- 
-     // B♯ = C
-     EXPECT_TRUE(t.isEnharmonicEquivalent(-1, 2));
- 
-     // C♯ != D♭
-     t = Transposer(0, 1, false, 19, edo19Major);
-     EXPECT_FALSE(t.isEnharmonicEquivalent(1, -1));
-     // but one more flat is equivalent
-     EXPECT_TRUE(t.isEnharmonicEquivalent(1, -2));
+ TEST(TransposerTest, ChromaticTransposeFromRandomBasesWithExpectedResults)
+ {
+    struct IntervalTestCase {
+        int interval;
+        int alteration;
+    };
+
+    struct State {
+        int displacement;
+        int alteration;
+    };
+
+    // 1. Interval + alteration to apply
+    const std::array<IntervalTestCase, 7> intervals = {{
+        { 0,  0 },  // unison
+        { 1, -1 },  // minor 2nd
+        { 2,  1 },  // augmented 3rd
+        { 3,  0 },  // perfect 4th
+        { 4, -2 },  // doubly diminished 5th
+        { 5,  0 },  // major 6th
+        { 6,  1 },  // augmented 7th
+    }};
+
+    // 2. Starting base values
+    const std::array<State, 7> bases = {{
+        { -5,  2 },  // E♭♭
+        {  0, -1 },  // C♭
+        { 12,  0 },  // A
+        {  3, -3 },  // F♭♭♭
+        { -9,  1 },  // A♯
+        {  7,  0 },  // C
+        { 14, -2 },  // C♭♭
+    }};
+
+    // 3. Expected results after transposition
+    const std::array<State, 7> expectedResults = {{
+        { -5,  2 },   // unison
+        {  1, -2 },   // D♭ from C♭
+        { 14,  2 },   // C♯♯ from A
+        {  6, -4 },   // B♭♭♭♭ from F♭♭♭
+        { -5, -1 },   // E♭ from A♯
+        { 12, 0 },    // A from C
+        { 20, -1 },   // B♭ from C♭♭
+    }};
+
+    for (size_t i = 0; i < intervals.size(); ++i) {
+        const auto& [interval, alteration] = intervals[i];
+        const auto& [baseDisp, baseAlt] = bases[i];
+        const auto& [expectedDisp, expectedAlt] = expectedResults[i];
+
+        SCOPED_TRACE("Case #" + std::to_string(i) +
+                     ", Interval = " + std::to_string(interval) +
+                     ", Alteration = " + std::to_string(alteration));
+
+        Transposer t(baseDisp, baseAlt);
+        t.chromaticTranspose(interval, alteration);
+
+        EXPECT_EQ(t.displacement(), expectedDisp);
+        EXPECT_EQ(t.alteration(), expectedAlt);
+
+        t.chromaticTranspose(-interval, -alteration);
+        EXPECT_EQ(t.displacement(), baseDisp);
+        EXPECT_EQ(t.alteration(), baseAlt);
+    }
+}
+
+TEST(TransposerTest, Non12EDOEquivalentPitch) {
+    std::vector<int> edo19Major = {0, 3, 6, 8, 11, 14, 17};
+    Transposer t(0, 0, false, 19, edo19Major);
+
+    // D♭ != C
+    EXPECT_FALSE(t.isEnharmonicEquivalent(1, -2));
+    // but one more flat is equivalent
+    EXPECT_TRUE(t.isEnharmonicEquivalent(1, -3));
+
+    // B♯ = C
+    EXPECT_TRUE(t.isEnharmonicEquivalent(-1, 2));
+
+    // C♯ != D♭
+    t = Transposer(0, 1, false, 19, edo19Major);
+    EXPECT_FALSE(t.isEnharmonicEquivalent(1, -1));
+    // but one more flat is equivalent
+    EXPECT_TRUE(t.isEnharmonicEquivalent(1, -2));
 }
  
  TEST(TransposerTest, SimplifySpellingWorks) {
