141da160434b0a7a4a2bb55ac040dc312482ee3f
rpatters1 <rpatters1@users.noreply.github.com>
2025-08-18 00:04:08 +0000

    deploy: 4df20f11d03071e9307e854a70e20099de70ca75

--- classmusx_1_1dom_1_1options_1_1MiscOptions.js
+++ classmusx_1_1dom_1_1options_1_1MiscOptions.js
@@ -3,9 +3,9 @@ var classmusx_1_1dom_1_1options_1_1MiscOptions =
     [ "MiscOptions", "classmusx_1_1dom_1_1options_1_1MiscOptions.html#a4b2b3aa959c6ad0d0551c6e9f8d53a47", null ],
     [ "alignMeasureNumbersWithBarlines", "classmusx_1_1dom_1_1options_1_1MiscOptions.html#a5e0d78c81d636b9da7bf9fc29268df82", null ],
     [ "consolidateRestsAcrossLayers", "classmusx_1_1dom_1_1options_1_1MiscOptions.html#a468b44c8ac1c4c9b15a73f333432968b", null ],
+    [ "keepWrittenOctaveInConcertPitch", "classmusx_1_1dom_1_1options_1_1MiscOptions.html#a8cf93fee9d505c83ed107be980a8f5e3", null ],
     [ "shapeDesignerDashLength", "classmusx_1_1dom_1_1options_1_1MiscOptions.html#af2732b25eab7eb1991e180c5ec155568", null ],
     [ "shapeDesignerDashSpace", "classmusx_1_1dom_1_1options_1_1MiscOptions.html#aba6017ce6ed80dd7b154bf9ed2dd8c81", null ],
     [ "showActiveLayerOnly", "classmusx_1_1dom_1_1options_1_1MiscOptions.html#a600fb6f99f3619c809db467017822fb1", null ],
-    [ "showRepeatsForParts", "classmusx_1_1dom_1_1options_1_1MiscOptions.html#a24a27d948cdc5c208a086fafaa7356eb", null ],
-    [ "showSoundingOctaveInConcertPitch", "classmusx_1_1dom_1_1options_1_1MiscOptions.html#a2621e6b77a6d68b4014918347ff49889", null ]
+    [ "showRepeatsForParts", "classmusx_1_1dom_1_1options_1_1MiscOptions.html#a24a27d948cdc5c208a086fafaa7356eb", null ]
 ];
